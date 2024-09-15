9810b52f9fa73c62579feb7221ba928a9728dff5
GonzaSanabria <gonzanabriagustin@gmail.com>
2024-09-15 20:45:24 -0300

    Finalizo el ejercicio 3.

--- Trabajo practico 1/ejercicio_numero3&4.py
+++ Trabajo practico 1/ejercicio_numero3&4.py
@@ -80,3 +80,63 @@ while True:
             print("Vehículo no encontrado. Por favor ingrese uno de los vehículos disponibles: Auto, Moto o Bicicleta.")
     except ValueError:
         print("Entrada no válida. Por favor ingrese un valor correcto.")
+
+print("Día   Aula")
+print("===================")
+
+
+for dia in range(1, 7):
+    
+    if dia % 2 == 0:
+        aula = "A-300"  
+    else:
+        aula = "A-315"  
+
+    
+    print(f"{dia}    {aula}")
+
+print("Carga de edades.")
+contError = 0 
+while True:
+    try:
+        edad = int(input("Ingrese una edad mayor o igual a 18: "))  
+        if edad >= 18:
+            print(f"La edad ingresada es: {edad}")
+            break  
+        else:
+            print("Debe ingresar una edad mayor o igual a 18.")
+            contError += 1
+    except ValueError:
+        print("Por favor, ingrese un número válido.")
+
+print(f"Se ha ingresado la edad erroneamente: {contError}")
+
+print("\nPROMEDIO DE NOTAS")
+
+sumNotas = 0
+prom = 0
+for notaNum in range(1, 6):
+    while True:
+        try:
+            nota = int(input("Ingrese una nota del 1 al 10: "))
+            if nota > 0 and nota <= 10:
+                sumNotas += nota
+                break
+            else:
+                print("Por favor, ingrese un numero del 1 al 10.")
+        except ValueError:
+            print("Por favor ingrese numeros. Intente nuevamente.")
+
+prom = sumNotas / 5
+
+print(f"El promedio de notas es: {prom}")
+print("\nCOSTO DEL COMEDOR.")
+
+
+print(f"Día   Costo\n{'=' * 20}")
+
+
+for dia in range(1, 7):
+    
+    print(f"El costo del comedor en el dia {dia} es de: ${1250 * dia} ")
+    
