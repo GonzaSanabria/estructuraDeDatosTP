5fe7bbd3e4b33a2e5439866c3c64c17b395d2e7f
GonzaSanabria <gonzanabriagustin@gmail.com>
2024-09-15 21:31:05 -0300

    Finalizo el ejercicio 2 del TP2

---
+++ Trabajo practico 2/ejercicioNumero2.py
@@ -0,0 +1,12 @@
+def fechaValida(num1:int, num2:int, num3:int)->bool:
+    dias = list(range(1, 31))
+    meses = list(range(1, 13))
+    
+    if num1 in dias and num2 in meses and num3 <= 2024:
+        print(True)
+    else:
+        print(False)
+    
+
+fechaValida(20, 3, 2025)
+
