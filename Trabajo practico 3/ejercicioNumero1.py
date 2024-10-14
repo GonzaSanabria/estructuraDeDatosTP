8c99150d71fffebe020fb645e500acec44106d2b
GonzaSanabria <gonzanabriagustin@gmail.com>
2024-10-14 19:51:20 -0300

    Una manera iterativa de hacer el mismo ejercicio.

--- Trabajo practico 3/ejercicioNumero1.py
+++ Trabajo practico 3/ejercicioNumero1.py
@@ -6,5 +6,19 @@ def palabraInvertida(palabra: str):
 
 prueba = palabraInvertida("neuquen")
 
+# Otra manera de hacerlo
 
+def palindromo(palabra: str): 
+    palabra = palabra.lower() 
+    longitud = len(palabra) # Con esto definimos la longitud de la palabra 
+
+    for i in range(longitud // 2): # Con esto solo analizamos la mitad de la palabra
+        if palabra[i] != palabra[longitud - i - 1]: # En esta parte se hacen las comparaciones
+            # con palabra[i] analizamos la palabra normalmente y con palabra[longitud - i - 1] analizamos la palabra al revez. 
+            print("No es palindromo.")
+            return # Si las mitades son iguales de atras para adelante, esta condicional no retorna nada. 
+        
+    print("Es palindromo X2.")
+
+pruebaFuncion = palindromo("neuquen")
 
